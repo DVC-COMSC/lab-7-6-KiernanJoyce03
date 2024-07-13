d7c41f5ee72601d8fbe0a07e1f9596dfa23a625f
KiernanJoyce03 <kiernanmj007@gmail.com>
2024-07-12 23:17:24 -0700

    first commit

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ def getInput():
     Code Your Program here
     ########################################
     """
-
+    userInput = input("Input a list of numbers ")
+    numbers = list(userInput.split())
+    for i in range (len(numbers)):
+        numbers[i] = int(numbers[i])
+    
+    return numbers
+    
 
 def makeReverse(numbers):
     """
@@ -13,6 +19,18 @@ def makeReverse(numbers):
     Code Your Program here
     ########################################
     """
+    N = len(numbers)
+    ret = []
+    for i in range (N ):
+        
+        ret.insert(0,numbers[0])
+        numbers.pop(0)
+        
+
+        
+    
+    return ret
+    
 
 
 def main():
